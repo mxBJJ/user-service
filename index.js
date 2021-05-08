e47034c268d41b31927316768d19b102047ccc47
Max Mendes <maxmendes0509@gmail.com>
2021-05-08 17:23:05 -0300

    first deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const router = require('./routes')
+const router = require('./src/routes')
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
 
-app.listen(3000, () => {
+const port = process.env.PORT || '3030'
+
+app.listen(port, () => {
     console.log('Server running...')
 })
